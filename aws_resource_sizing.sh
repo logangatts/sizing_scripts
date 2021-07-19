6e00c5fa12e3dd52dfdfab1c79fb561b3a58098a
logangatts <76017939+logangatts@users.noreply.github.com>
2021-07-19 16:24:13 -0500

    Add files via upload

---
+++ aws_resource_sizing.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+# Script to fetch AWS inventory for Lacework sizing.
+# Requirements: awscli, jq
+
+# You can specify a profile with the -p flag, or get JSON output with the -j flag.
+# Note:
+# 1. You can specify multiple accounts by passing a comma seperated list, e.g. "default,qa,test",
+# there are no spaces between accounts in the list
+# 2. The script takes a while to run in large accounts with many resources, the final count is an aggregation of all resources found.
+
+
+AWS_PROFILE=default
+
+# Usage: ./lw_aws_inventory.sh
+while getopts ":jp:" opt; do
+  case ${opt} in
+    p )
+      AWS_PROFILE=$OPTARG
+      ;;
+    j )
+      JSON="true"
+      ;;
+    \? )
+      echo "Usage: ./lw_aws_inventory.sh [-p profile] [-j]" 1>&2
+      exit 1
+      ;;
+    : )
+      echo "Usage: ./lw_aws_inventory.sh [-p profile] [-j]" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Set the initial counts to zero.
+EC2_INSTANCES=0
+RDS_INSTANCES=0
+REDSHIFT_CLUSTERS=0
+ELB_V1=0
+ELB_V2=0
+NAT_GATEWAYS=0
+LAMBDA=0
+LAMBDA_EXEC=0
+S3=0
+
+function getRegions {
+  aws --profile $profile ec2 describe-regions --output json | jq -r '.[] | .[] | .RegionName'
+}
+
+function getInstances {
+  aws --profile $profile ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId]' --region $r --output json --no-paginate | jq 'flatten | length'
+}
+
+function getRDSInstances {
+  aws --profile $profile rds describe-db-instances --region $r --output json --no-paginate | jq '.DBInstances | length'
+}
+
+function getRedshift {
+  aws --profile $profile redshift describe-clusters --region $r --output json --no-paginate | jq '.Clusters | length'
+}
+
+function getElbv1 {
+  aws --profile $profile elb describe-load-balancers --region $r  --output json --no-paginate | jq '.LoadBalancerDescriptions | length'
+}
+
+function getElbv2 {
+  aws --profile $profile elbv2 describe-load-balancers --region $r --output json --no-paginate | jq '.LoadBalancers | length'
+}
+
+function getNatGateways {
+  aws --profile $profile ec2 describe-nat-gateways --region $r --output json --no-paginate | jq '.NatGateways | length'
+
+}
+
+function getlambdas {
+  aws --profile $profile lambda list-functions  --region $r --output json --no-paginate | jq '.[] | length'
+}
+
+function gets3 {
+  aws s3api list-buckets | jq '.Buckets | length'
+}
+
+function calculateInventory {
+  profile=$1
+  for r in $(getRegions); do
+    if [ "$JSON" != "true" ]; then
+      echo $r
+    fi
+    instances=$(getInstances $r $profile)
+    EC2_INSTANCES=$(($EC2_INSTANCES + $instances))
+
+    rds=$(getRDSInstances $r $profile)
+    RDS_INSTANCES=$(($RDS_INSTANCES + $rds))
+
+    redshift=$(getRedshift $r $profile)
+    REDSHIFT_CLUSTERS=$(($REDSHIFT_CLUSTERS + $redshift))
+
+    elbv1=$(getElbv1 $r $profile)
+    ELB_V1=$(($ELB_V1 + $elbv1))
+
+    elbv2=$(getElbv2 $r $profile)
+    ELB_V2=$(($ELB_V2 + $elbv2))
+
+    natgw=$(getNatGateways $r $profile)
+    NAT_GATEWAYS=$(($NAT_GATEWAYS + $natgw))
+
+    lambdainv=$(getlambdas $r $profile)
+    LAMBDA=$(($LAMBDA + $lambdainv))
+done
+
+#s3 is global so only needs to be run once for accurate counts (not per region)
+S3= $(gets3)
+
+TOTAL=$(($EC2_INSTANCES + $RDS_INSTANCES + $REDSHIFT_CLUSTERS + $ELB_V1 + $ELB_V2 + $NAT_GATEWAYS + $LAMBDA + $S3))
+}
+
+function textoutput {
+  echo "######################################################################"
+  echo "Lacework inventory collection complete."
+  echo ""
+  echo "EC2 Instances:     $EC2_INSTANCES"
+  echo "RDS Instances:     $RDS_INSTANCES"
+  echo "Redshift Clusters: $REDSHIFT_CLUSTERS"
+  echo "v1 Load Balancers: $ELB_V1"
+  echo "v2 Load Balancers: $ELB_V2"
+  echo "NAT Gateways:      $NAT_GATEWAYS"
+  echo "Lambdas:           $LAMBDA"
+  echo "S3 Buckets:        $S3"
+  echo "===================="
+  echo "Total Resources:   $TOTAL"
+}
+
+function jsonoutput {
+  echo "{"
+  echo "  \"ec2\": \"$EC2_INSTANCES\","
+  echo "  \"rds\": \"$RDS_INSTANCES\","
+  echo "  \"redshift\": \"$REDSHIFT_CLUSTERS\","
+  echo "  \"v1_lb\": \"$ELB_V1\","
+  echo "  \"v2_lb\": \"$ELB_V2\","
+  echo "  \"nat_gw\": \"$NAT_GATEWAYS\","
+  echo "  \"lambdainv\": \"$LAMBDA\","
+  echo "  \"s3inv\":  \"$S3\","
+  echo "  \"total\": \"$TOTAL\""
+  echo "}"
+}
+
+for PROFILE in $(echo $AWS_PROFILE | sed "s/,/ /g")
+do
+    calculateInventory $PROFILE
+done
+
+if [ "$JSON" == "true" ]; then
+  jsonoutput
+else
+  textoutput
+fi
